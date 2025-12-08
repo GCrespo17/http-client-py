8d070389974a5e68befa890af63ff94fddb0cfbb
gcrespo <gcrespo1705@gmail.com>
2025-12-08 18:25:32 -0400

    refactored connection and send request functions

--- client.py
+++ client.py
@@ -3,25 +3,31 @@ import urllib.parse
 
 conn = None
 
-def connectToHostGetMethod(host):
+def connectToHost(host, method):
     global conn
     conn = http.client.HTTPSConnection(host)
-    conn.request("GET", "/", headers = {"Host": host})
+    conn.request(method, "/", headers = {"Host": host})
     return conn
 
-def sendRequestGetMethod(host):
+def sendRequest(host, method):
     global conn
     print(f"HTTP request Sent to {host}")
-    conn = connectToHostGetMethod(host)
+    if(method == "GET"):
+        conn = connectToHost(host, method)
+    elif(method == "HEAD"):
+        conn = connectToHost(host, method)
     response = conn.getresponse()
     print(response.status, response.reason)
-    print(f"Content-Type: {response.getheader("Content-Type")}")
-    print(f"Server: {response.getheader("Server")}")
-    print(f"Date: {response.getheader("Date")}")
-    print(f"Content-Length: {response.getheader("Content-Length")}") 
-    print(f"Cache-Control: {response.getheader("Cache-Control")}")
+    print(f"Content-Type: {response.getheader('Content-Type')}")
+    print(f"Server: {response.getheader('Server')}")
+    print(f"Date: {response.getheader('Date')}")
+    print(f"Content-Length: {response.getheader('Content-Length')}") 
+    print(f"Cache-Control: {response.getheader('Cache-Control')}")
     return response
     
+
+    
+
 
 def closeConnection():
     conn.close()
