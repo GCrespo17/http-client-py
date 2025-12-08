bf662985eb4ba5f7e3b9b9a0f84705476612b428
gcrespo <gcrespo1705@gmail.com>
2025-12-08 19:31:16 -0400

    refactorized most functions

--- client.py
+++ client.py
@@ -1,34 +1,32 @@
 import http.client
-import urllib.parse
 
-conn = None
+class HTTPClient:
+    def __init__(self):
+        self.conn = None
 
-def connectToHost(host, method):
-    global conn
-    conn = http.client.HTTPSConnection(host)
-    conn.request(method, "/", headers = {"Host": host})
-    return conn
+    def connectToHost(self, host, method):
+        self.conn = http.client.HTTPSConnection(host)
+        self.conn.request(method, "/", headers = {"Host": host})
+        return self.conn
 
-def sendRequest(host, method):
-    global conn
-    print(f"HTTP request Sent to {host}")
-    if(method == "GET"):
-        conn = connectToHost(host, method)
-    elif(method == "HEAD"):
-        conn = connectToHost(host, method)
-    response = conn.getresponse()
-    print(response.status, response.reason)
-    print(f"Content-Type: {response.getheader('Content-Type')}")
-    print(f"Server: {response.getheader('Server')}")
-    print(f"Date: {response.getheader('Date')}")
-    print(f"Content-Length: {response.getheader('Content-Length')}") 
-    print(f"Cache-Control: {response.getheader('Cache-Control')}")
-    return response
+    def sendRequest(self, host, method):
+        self.connectToHost(host, method)
+        response = self.conn.getresponse() 
+        return response
+
+    def closeConnection(self):
+        if self.conn:
+            self.conn.close()
+
+
+
+
+
+
+
+        
+
     
 
-    
 
-
-def closeConnection():
-    conn.close()
 
---
+++ gui.py
@@ -0,0 +1,104 @@
+from tkinter import *
+from tkinter import ttk
+import client
+
+httpClient = client.HTTPClient()
+
+sendUrlEntry = None
+option = None
+headerText = None
+bodyText = None
+
+
+#Formatear mi encabezado para que los ojos no sufran
+def formatHeaders(headers, status, reason, version):
+    httpVersion = f"HTTP/{version // 10}.{version % 10}"
+    formattedText = f"{httpVersion} {status} {reason}\n\n"
+    for key, value in headers:
+        formattedText += f"{key}: {value}\n\n"
+    return formattedText
+
+#Imprime la informacion dada
+def printHeaderTextInfo(header):
+    headerText.config(state = NORMAL)
+    headerText.delete("1.0", END)
+    headerText.insert("1.0", header)
+    headerText.config(state = DISABLED)
+
+def printBodyTextInfo(body):
+    bodyText.config(state = NORMAL)
+    bodyText.delete("1.0", END)
+    bodyText.insert("1.0", body)
+    bodyText.config(state = DISABLED)
+
+#Funcion que se activa cuando se presiona el boton de enviar solicitud
+def pressSendRequestBtn():
+    urlContent = sendUrlEntry.get()
+    response = httpClient.sendRequest(urlContent, option.get())
+    headers = formatHeaders(response.getheaders(), response.status, response.reason, response.version)
+    body = response.read()
+    printHeaderTextInfo(headers)
+    printBodyTextInfo(body)
+    httpClient.closeConnection()
+    
+
+#Creamos la ventana inicial
+def createWindow(title, size):
+    window = Tk()
+    window.title(title)
+    window.geometry(size)
+    createFormFrame(window)
+    createHeaderFrame(window)
+    createBodyFrame(window)
+    return window
+
+
+#Creamos el frame donde estara el form para introducir los datos
+def createFormFrame(window):    
+    formFrame = Frame(window, relief = "groove")
+    formFrame.pack()
+    urlLabel = Label(formFrame, text = "URL: ").grid(row = 0, column = 0, padx = 5, pady = 5)
+    
+    global sendUrlEntry
+    sendUrlEntry = Entry(formFrame)
+    sendUrlEntry.grid(row = 0, column = 1, padx = 5, pady = 5)
+    sendUrlEntry.config(width = 50)
+    
+    global option
+    option = StringVar()
+    methodComboBox = ttk.Combobox(formFrame, textvariable = option, values = ["GET", "HEAD"], state = "readonly")
+    methodComboBox.current(0)
+    methodComboBox.grid(row = 0, column = 2, padx = 5, pady = 5)
+    
+    sendRequestBtn = Button(formFrame, text = "Enviar Solicitud")
+    sendRequestBtn.config(command = pressSendRequestBtn)
+    sendRequestBtn.config(activebackground = '#809096')
+    sendRequestBtn.grid(row = 0, column = 3, padx = 5, pady = 5)
+
+
+#Creamos el frame donde estara la cabecera de la respuesta del host
+def createHeaderFrame(window):
+    headerFrame = Frame(window, relief = "groove")
+    headerFrame.pack()
+    headerResponseLabel = Label(headerFrame, text = "Cabeceras de Respuesta: ")
+    headerResponseLabel.grid(row = 0, column = 0, padx = 5, pady = 5)
+    
+    global headerText
+    headerText = Text(headerFrame, width = 90, height = 15)
+    headerText.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+
+#Creamos el frame donde estara el cuerpo de la respuesta del host
+def createBodyFrame(window):
+    bodyFrame = Frame(window, relief = "groove")
+    bodyFrame.pack()
+    bodyResponseLabel = Label(bodyFrame, text = "Cuerpo/Contenido: ")
+    bodyResponseLabel.grid(row = 0, column = 0, padx = 5, pady = 5)
+    
+    global bodyText
+    bodyText = Text(bodyFrame, width = 90, height = 15)
+    bodyText.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+
+
+
